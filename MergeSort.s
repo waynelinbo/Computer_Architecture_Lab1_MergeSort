fcec90d682763e6fd63c07bd49d7874bda2d11e1
waynelinbo <39195011+waynelinbo@users.noreply.github.com>
2020-10-13 06:34:00 +0800

    Update MergeSort.s

--- MergeSort.s
+++ MergeSort.s
@@ -54,28 +54,33 @@ main:
 # s3 : mid = (start+end)/2
 
 mergeSort:
+	# sp    : ra
+	# sp+4  : &addr
+	# sp+8  : start
+	# sp+12 : end
+	# sp+16 : mid
 	addi 	sp, sp, -20
 	sw	ra, 0(sp)
 	
-	# copy argument
-	addi	s0, a0, 0
-	addi	s1, a1, 0
-	addi	s2, a2, 0
+	# copy argument to saved register
+	addi	s0, a0, 0	# s0 : &addr
+	addi	s1, a1, 0	# s1 : start
+	addi	s2, a2, 0	# s2 : end
 
-	# if(start<end), else go to label(endMetgeSort)
+	# if(start<end), else go to label(endMergeSort)
 	bge	s1, s2, endMergeSort
 
 	# use substract to implement divide
 	# s3 <= mid = (start+end)/2
-	add	s3, s1, s2
-	addi	s4, x0, 0
+	add	s3, s1, s2	# mid(s3) = start(s1) + end(s2)
+	addi	s4, x0, 0	# (s4+1) each time when (s3-2) until s3 < 0
 div:
 	blt	s3, x0, endDiv
-	addi	s3, s3, -2
-	addi	s4, s4, 1
+	addi	s3, s3, -2	# each time s3 - 2
+	addi	s4, s4, 1	# each time s4 + 1
 	jal	x0, div		
 endDiv:
-	addi	s3, s4, -1
+	addi	s3, s4, -1	# s3 = mid : floor((start+end)/2)
 	
 	# save &addr(s0), start(s1), end(s2), mid(s3) to mem
 	sw	s0, 4(sp)
@@ -85,25 +90,25 @@ endDiv:
 	
 	# prepare argument to do left mergeSort
 	# mergeSort(&arr(a0), start(a1), mid(a2))
-	addi	a0, s0, 0
-	addi	a1, s1, 0
-	addi	a2, s3, 0
+	addi	a0, s0, 0	# a0 <= &addr (s0)
+	addi	a1, s1, 0	# a1 <= start (s1)
+	addi	a2, s3, 0	# a2 <= mid   (s3)
 	jal	ra, mergeSort
 
 	# prepare argument to do right mergeSort
 	# mergeSort(&arr(a0), mid(a1)+1, end(a2))
-	lw	a0, 4(sp)
-	lw	a1, 16(sp)
-	addi	a1, a1, 1
-	lw	a2, 12(sp)
+	lw	a0, 4(sp)	# a0 <= $addr	(sp+4)
+	lw	a1, 16(sp)	# a1 <= mid	(sp+16)
+	addi	a1, a1, 1	# a1 <= mid + 1
+	lw	a2, 12(sp)	# a2 <= end	(sp+12)
 	jal	ra, mergeSort
 
 	# prepare argument to do merge
 	# mergeSort(&arr(a0), start(a1), mid(a2), end(a3))
-	lw	a0, 4(sp)
-	lw	a1, 8(sp)
-	lw	a2, 16(sp)
-	lw	a3, 12(sp)
+	lw	a0, 4(sp)	# a0 <= $addr	(sp+4)
+	lw	a1, 8(sp)	# a1 <= start	(sp+8)
+	lw	a2, 16(sp)	# a2 <= mid	(sp+16)
+	lw	a3, 12(sp)	# a3 <= end	(sp+12)
 	jal	ra, merge
 
 endMergeSort:	
@@ -122,56 +127,60 @@ endMergeSort:
 # a3 : end
 
 merge:
-	# we want to copy array[start...end] from memory to stack
-
-
+	# 0. we have left sorted array(start..mid) & right sorted array(mid+1..end)
+	# 1. copy array[start..end] from memory to stack
+	# 2. use 2 index (left: from 0, right : from mid-start+1) to compare left & right in stack
+	# 3. write the smaller back to memory, and the smaller index + 1
+	# 4. check is there any index have reached(>) the max index(left: mid-start, right: end-start)
+	# 5. if no, go back to step 2; else write the left/right left value back to memory
+	
 	# calculate how many stack space we need
 	# t0 : total count of index 	=> end - start + 1
 	# t1 : total space of memsize 	=> t0*4
 	# t2 : 2's complement of t1 	=> -t1
-	sub	t0, a3, a1
-	addi	t0, t0, 1
-	add	t1, t0, t0
-	add	t1, t1, t1
-	xori	t2, t1, 0xffffffff  		# 2's complement  + -> -
-	addi	t2, t2, 1			# 2's complement  + -> -
+	sub	t0, a3, a1			# total(t0) = end(a3) - start(a1)
+	addi	t0, t0, 1			# total(t0) = end(a3) - start(a1) + 1
+	add	t1, t0, t0			# space(t1) = total(t0)*2
+	add	t1, t1, t1			# space(t1) = space(t1)*2 = total(t0)*4
+	xori	t2, t1, 0xffffffff  		# 1's complement  + -> -, t1 -> t2
+	addi	t2, t2, 1			# 2's complement(t2)  => 1's complement(t2) + 1
 	add	sp, sp, t2
 
 
-	# read array[start...end] from memory to stack
+	# copy array[start..end] from memory to stack
 	# t2 : current index of new array(stack)
 	# t3 : current index of old array(memory) 
-	addi	t3, a1, 0			# start from start(a1)
-	addi	t2, x0, 0			# start from 0
+	addi	t3, a1, 0			# memory index(t3) start from start(a1)
+	addi	t2, x0, 0			# stack index(t2) start from 0
 read2Stack:
-	blt	a3, t3, endRead			# if(t3 < end(a3)), else go to label(endRead)
-	add	t4, t3, t3
-	add	t4, t4, t4
-	add	t4, a0, t4			# calculate old array(memory) mem address
-	lw	t5, 0(t4)			# load to t5
-	add	t6, t2, t2
-	add	t6, t6, t6
-	add	t6, sp ,t6			# calculate new array(stack) mem address
-	sw	t5, 0(t6)			# store t5 to new array(stack)
-	addi	t2, t2, 1			# new array index + 1
-	addi	t3, t3, 1			# old array index + 1
+	blt	a3, t3, endRead			# if(mem_index(t3) < end(a3)), else go to label(endRead)
+	add	t4, t3, t3			# offset_space(t4) = mem_index(t3)*2
+	add	t4, t4, t4			# offset_space(t4) = offset_space(t4)*2 = mem_index(t3)*4
+	add	t4, a0, t4			# mem_addr(t4) = &addr(a0) + offset_space(t4) 
+	lw	t5, 0(t4)			# t5 = *mem_addr(t4)
+	add	t6, t2, t2			# offset_space(t6) = st_index(t2)*2
+	add	t6, t6, t6			# offset_space(t6) = offset_space(t6)*2 = st_index(t2)*4
+	add	t6, sp ,t6			# st_addr(t6) = initial_addr(sp) + offset_space(t6) 
+	sw	t5, 0(t6)			# *st_addr(t6) = t5
+	addi	t2, t2, 1			# st_index + 1
+	addi	t3, t3, 1			# mem_index + 1
 	jal	x0, read2Stack
 endRead:
 
 	
 	# do merge
-	# view new array(stack) as two parts, compare each, then store to old array(mem) from little to big
+	# view stack as two parts(left sorted & right sorted), compare each, then store to memory from little to big
 	# prepare some variable
 	# t2 : left part index  				=> 0
 	# t3 : right part index					=> mid(a2) - start(a1) + 1 => t4 + 1
 	# t4 : left terminate index(when t2 > t4)		=> mid(a2) - start(a1)
 	# t5 : right terminate index(when t3 > t5)		=> end(a3) - start(a1)
 	# t6 : write back index of old array(memory)		=> start(a1)
-	sub	t4, a2, a1 			# left max
-	sub	t5, a3, a1			# right max
-	addi	t2, x0, 0			# left index
-	addi	t3, t4, 1			# right index
-	addi	t6, a1, 0			# &addr current save index
+	sub	t4, a2, a1 			# left max(t4) = mid(a2) - start(a1)
+	sub	t5, a3, a1			# right max(t5) = end(a3) - start(a1)
+	addi	t2, x0, 0			# left index(t2) = start form 0
+	addi	t3, t4, 1			# right index(t3) = start from mid(a2) - start(a1) + 1
+	addi	t6, a1, 0			# t6 : write back index from start(a1)
 
 
 	# if((left index <= left terminate) && (right index <= right terminate)), else go to label(endMergeLoop)
@@ -183,66 +192,66 @@ endRead:
 	# t1 : calculate the right mem, then load value in it
 	# compare which is smaller, then save the smaller in old array(memory)
 mergeLoop:
-	slt	t0, t4, t2				# get t0
-	slt	t1, t5, t3				# get t1
-	or	t0, t0, t1
-	xori	t0, t0, 0x1
-	beq	t0, x0, endMergeLoop			# if((t0||t1) == 0), else go to label(endMergeLoop)
-	add	t0, t2, t2						
-	add	t0, t0, t0
-	add	t0, sp, t0				# calculate the left part mem address
-	lw	t0, 0(t0)				# load to t0
-	add	t1, t3, t3
-	add	t1, t1, t1
-	add	t1, sp, t1				# calculate the right part mem address
-	lw	t1, 0(t1)				# load to t1
-	blt	t1, t0,  rightSmaller			# if(t0<=t1), else go to label(rightSmaller)
-	add	t1, t6, t6
-	add	t1, t1, t1
-	add	t1, a0, t1				# calculate the write back address in old array(memory)
-	sw	t0, 0(t1)				# save t0 to old array(memory)
-	addi	t6, t6, 1				# write back index + 1
-	addi	t2, t2, 1				# left part index + 1
+	slt	t0, t4, t2				# t0 = (left terminate(t4) < left index(t2))
+	slt	t1, t5, t3				# t1 = (right terminate(t5) < right index(t3))
+	or	t0, t0, t1				# t0 = t0 || t1
+	xori	t0, t0, 0x1				# t0 = ~t0
+	beq	t0, x0, endMergeLoop			# if((t0||t1) != 0), else go to label(endMergeLoop)
+	add	t0, t2, t2				# offset_space(t0) = left_index(t2)*2
+	add	t0, t0, t0				# offset_space(t0) = offset_space(t0)*2 = left_index(t2)*4
+	add	t0, sp, t0				# left_addr(t0) = initial_addr(sp) + offset_space(t0)
+	lw	t0, 0(t0)				# left_value(t0) = *left_addr(t0)
+	add	t1, t3, t3				# offset_space(t1) = right_index(t3)*2
+	add	t1, t1, t1				# offset_space(t1) = offset_space(t1)*2 = right_index(t3)*4
+	add	t1, sp, t1				# right_addr(t1) = initial_addr(sp) + offset_space(t1)
+	lw	t1, 0(t1)				# right_value(t1) = *right_addr(t1)
+	blt	t1, t0,  rightSmaller			# if(left_value(t0)<=right_value(t1)), else go to label(rightSmaller)
+	add	t1, t6, t6				# offset_space(t1) = mem_index(t6)*2
+	add	t1, t1, t1				# offset_space(t1) = offset_space(t1)*2 = mem_index(t6)*4
+	add	t1, a0, t1				# mem_addr(t1) = &addr(a0) + offset_space(t1)
+	sw	t0, 0(t1)				# *mem_addr(t1) = left_value(t0)
+	addi	t6, t6, 1				# mem_index + 1
+	addi	t2, t2, 1				# left_index + 1
 	jal	x0, mergeLoop
 rightSmaller:
-	add	t0, t6, t6
-	add	t0, t0, t0
-	add	t0, a0, t0				# calculate the write back address in old array(memory)
-	sw	t1, 0(t0)				# save t1 to old array(memory)
-	addi	t6, t6, 1				# write back index + 1
-	addi	t3, t3, 1				# left part index + 1
+	add	t0, t6, t6				# offset_space(t0) = mem_index(t6)*2
+	add	t0, t0, t0				# offset_space(t0) = offset_space(t0)*2 = mem_index(t6)*4
+	add	t0, a0, t0				# mem_addr(t0) = &addr(a0) + offset_space(t0)
+	sw	t1, 0(t0)				# *mem_addr(t0) = right_value(t1)
+	addi	t6, t6, 1				# mem_index + 1
+	addi	t3, t3, 1				# right_index + 1
 	jal	x0, mergeLoop
 endMergeLoop:
 
 
 	# if left part still have elements, store leave in to old array
 	# if right part still have elements, store leave in to old array
-	bge	t5, t3, rightLoop				# if(right index <= right terminate) => right part still have elements, go to label(rightLoop)
-leftLoop:							# else go on label(leftLoop)
-	add	t0, t2, t2
-	add	t0, t0, t0
-	add	t0, sp, t0					# calculate the left part mem address
-	lw	t0, 0(t0)					# load to t0
-	add	t1, t6, t6
-	add	t1, t1, t1
-	add	t1, a0, t1					# calculate the write back address in old array(memory)
-	sw	t0, 0(t1)					# save t0 to old array(memory)
-	addi	t6, t6, 1					# write back index + 1
-	addi	t2, t2, 1					# left part index + 1
-	bge	t4, t2, leftLoop				# check left part whether still have elements
+	bge	t5, t3, rightLoop			# if(right index <= right terminate) => right part still have elements, go to label(rightLoop)
+leftLoop:						# else go on label(leftLoop)
+	add	t0, t2, t2				# offset_space(t0) = left_index(t2)*2
+	add	t0, t0, t0				# offset_space(t0) = offset_space(t0)*2 = left_index(t2)*4
+	add	t0, sp, t0				# left_addr(t0) = initial_addr(sp) + offset_space(t0)
+	lw	t0, 0(t0)				# left_value(t0) = *left_addr(t0)
+	add	t1, t6, t6				# offset_space(t1) = mem_index(t6)*2
+	add	t1, t1, t1				# offset_space(t1) = offset_space(t1)*2 = mem_index(t6)*4
+	add	t1, a0, t1				# mem_addr(t1) = &addr(a0) + offset_space(t1)
+	sw	t0, 0(t1)				# *mem_addr(t1) = left_value(t0)
+	addi	t6, t6, 1				# mem_index + 1
+	addi	t2, t2, 1				# left_index + 1
+	bge	t4, t2, leftLoop			# check left part whether still have elements
 	jal	x0, endMerge
 rightLoop:
-	add	t1, t3, t3
-	add	t1, t1, t1
-	add	t1, sp, t1					# calculate the right part mem address
-	lw	t1, 0(t1)					# load to t1
-	add	t0, t6, t6
-	add	t0, t0, t0
-	add	t0, a0, t0					# calculate the write back address in old array(memory)
-	sw	t1, 0(t0)					# save t1 to old array(memory)
-	addi	t6, t6, 1					# write back index + 1
-	addi	t3, t3, 1					# right part index + 1
-	bge	t5, t3, rightLoop				# check right part whether still have elements
+	add	t1, t3, t3				# offset_space(t1) = right_index(t3)*2
+	add	t1, t1, t1				# offset_space(t1) = offset_space(t1)*2 = right_index(t3)*4
+	add	t1, sp, t1				# right_addr(t1) = initial_addr(sp) + offset_space(t1)
+	lw	t1, 0(t1)				# right_value(t1) = *right_addr(t1)
+	add	t0, t6, t6				# offset_space(t0) = mem_index(t6)*2
+	add	t0, t0, t0				# offset_space(t0) = offset_space(t0)*2 = mem_index(t6)*4
+	add	t0, a0, t0				# mem_addr(t0) = &addr(a0) + offset_space(t0)
+	sw	t1, 0(t0)				# *mem_addr(t0) = right_value(t1)
+	addi	t6, t6, 1				# mem_ndex + 1
+	addi	t3, t3, 1				# right_index + 1
+	bge	t5, t3, rightLoop			# check right part whether still have elements
 	jal	x0, endMerge
 
 
@@ -250,10 +259,10 @@ rightLoop:
 	# t0 : total count of index 	=> end - start + 1
 	# t1 : total space of memsize 	=> t0*4
 endMerge:
-	sub	t0, a3, a1
-	addi	t0, t0, 1
-	add	t1, t0, t0
-	add	t1, t1, t1
+	sub	t0, a3, a1			# total(t0) = end(a3) - start(a1)
+	addi	t0, t0, 1			# total(t0) = end(a3) - start(a1) + 1
+	add	t1, t0, t0			# space(t1) = total(t0)*2
+	add	t1, t1, t1			# space(t1) = space(t1)*2 = total(t0)*4
 	add	sp, sp, t1
 
 	jalr	x0, ra, 0
